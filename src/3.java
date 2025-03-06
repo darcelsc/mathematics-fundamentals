d7df2ea67dbd177a81c6fd7df7ec57511faaf34c
darcelsc <darcel_sc@gmx.com>
2025-03-06 14:20:50 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,7 @@
+  public class Main {
+    public static void main(String[] args) {
+      int x = 3;
+      int y = 5;
+      System.out.println("The sum of " + x + " and " + y + " is " + (x+y));
+    }
+  }
