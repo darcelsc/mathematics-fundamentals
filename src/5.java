1cfe26b84a2293492118657e7d9b403f22e9d6b2
darcelsc <darcel_sc@gmx.com>
2025-03-08 09:47:25 +0000

    Add 5.java

---
+++ src/5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		if (x >= 0) {
+			System.out.println("The number is positive");
+		} else if (x < 0) {
+			System.out.println("The number is negative");
+		} else {
+			System.out.println("The number is zero");
+		}
+	}
+}
